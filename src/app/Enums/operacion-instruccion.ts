0e2c75086c5fab295701ab5bd8c32302bee1cccc
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2025-04-26 13:06:05 -0500

    Creacion del modelo Instruccion

--- src/app/Enums/operacion-instruccion.ts
+++ src/app/Enums/operacion-instruccion.ts
@@ -1,8 +1,18 @@
 export enum OperacionInstruccion {
   LOAD = 'CARGAR',
-  MUL = 'MULTIPLICAR',
   ADD = 'SUMAR',
   SUB = 'RESTAR',
+  MUL = 'MULTIPLICAR',
   DIV = 'DIVIDIR',
   MOVE = 'MOVER',
+  HALT = 'DETENER',
+  INC = 'INCREMENTAR',
+  AND = 'CONJUNCION',
+  OR = 'DISYUNCION',
+  NOT = 'NEGACION',
+  OUT = 'SALIDA',
+  IN = 'ENTRADA',
+  JMP = 'SALTO',
+  JNE = 'SALTO SI NO IGUAL',
+  CMP = 'COMPARAR'
 }
--- src/app/Models/alu.ts
+++ src/app/Models/alu.ts
@@ -1,46 +1,103 @@
-import { OperacionInstruccion } from "../Enums/operacion-instruccion";
+import { OperacionInstruccion } from '../Enums/operacion-instruccion';
 
 export class ALU {
   operacionAEjecutar: OperacionInstruccion | undefined;
   operando1: number = 0;
   operando2: number = 0;
 
-  ejecutarOperacion(tipoOperacion: OperacionInstruccion, operando1: number, operando2: number): number {
+  ejecutarOperacion(
+    tipoOperacion: OperacionInstruccion,
+    operando1: number,
+    operando2: number
+  ): number {
     this.operacionAEjecutar = tipoOperacion;
     this.operando1 = operando1;
     this.operando2 = operando2;
 
     switch (this.operacionAEjecutar) {
       case OperacionInstruccion.ADD:
-        return this.add(operando1, operando2);
+        return this.sumar(operando1, operando2);
       case OperacionInstruccion.SUB:
-        return this.sub(operando1, operando2);
+        return this.restar(operando1, operando2);
       case OperacionInstruccion.MUL:
-        return this.multiply(operando1, operando2);
+        return this.multiplicar(operando1, operando2);
       case OperacionInstruccion.DIV:
-        return this.divide(operando1, operando2);
+        return this.dividir(operando1, operando2);
+      case OperacionInstruccion.INC:
+        return this.incrementar(operando1, operando2);
+      case OperacionInstruccion.NOT:
+        return this.negar(operando1)
+      case OperacionInstruccion.AND:
+        return this.conjuncion(operando1, operando2);
+      case OperacionInstruccion.OR:
+        return this.disyuncion(operando1, operando2);
+      case OperacionInstruccion.CMP:
+        return this.comparar(operando1, operando2);
       default:
         return 0;
     }
   }
 
-  private add(operando1: number, operando2: number): number {
+  private sumar(operando1: number, operando2: number): number {
     return operando1 + operando2;
   }
 
-  private sub(operando1: number, operando2: number): number {
+  private restar(operando1: number, operando2: number): number {
     return operando1 - operando2;
   }
 
-  private multiply(operando1: number, operando2: number): number {
+  private multiplicar(operando1: number, operando2: number): number {
     return operando1 * operando2;
   }
 
-  private divide(operando1: number, operando2: number): number {
+  private dividir(operando1: number, operando2: number): number {
     try {
       return operando1 / operando2;
     } catch (error) {
       return 0;
+    }
+  }
+
+  private incrementar(operando1: number, operando2: number): number {
+    if (operando2 == undefined || operando2 == null || operando2 == 0){
+      operando2 = 1;
+    }
+    return operando1 += operando2;
+  }
+
+  private negar(operando1: number): number {
+    if(operando1 == 0){
+      return 1;
+    }else if(operando1 == 1){
+      return 0;
+    }else{
+      return operando1 * -1;
+    }
+  }
+
+  private conjuncion(operando1: number, operando2: number): number {
+    if(operando1 == 1 && operando2 == 1){
+      return 1;
+    }else{
+      return 0;
+    }
+  }
+
+  private disyuncion(operando1: number, operando2: number): number {
+    if(operando1 == 0 && operando2 == 0){
+      return 0;
+    }else{
+      return 1;
+    }
+  }
+
+  private comparar(operando1: number, operando2: number): number {
+    if(operando1 == operando2){
+      return 0;
+    }else if(operando1 > operando2){
+      return 1;
+    }else{
+      return -1;
     }
   }
 }
